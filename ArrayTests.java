74fa162c160f2f24b3dd75600dec3eda54676ade
ArgyleSocks <maximtar@gmail.com>
2022-10-13 17:07:37 -0700

    Check1

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,43 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testRealRIP() {
+    int[] input1 = { 3, 4, 5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5, 4, 3 }, input1);
+  }
+
+  @Test
+  public void testRealRev() {
+    int[] input1 = { 3, 4, 5 };
+    assertArrayEquals(new int[]{ 5, 4, 3 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageEmpty() {
+    double[] a = {};
+    double result = ArrayExamples.averageWithoutLowest(a);
+    assertEquals(0, result);
+  }
+
+  @Test
+  public void testAverageUnique() {
+    double[] a = {1, 2, 3, 4, -5};
+    double result = ArrayExamples.averageWithoutLowest(a);
+    assertEquals(10, result);
+  }
+
+    @Test
+  public void testAverageMany() {
+    double[] a = {1, 1, 2, 3, 4};
+    double result = ArrayExamples.averageWithoutLowest(a);
+    assertEquals(10, result);
   }
 }
---
+++ fixjava.bat
@@ -0,0 +1,3 @@
+@echo off
+
+PATH = C:\Program Files\Java\jdk-19\bin;%PATH%
